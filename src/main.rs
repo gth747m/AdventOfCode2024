f1aba87cea6bd0e96490366dba708a68376c96c3
Brian Elder <gth747m@gmail.com>
2024-12-07 16:02:25 -0500

    Completed day 3.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day01;
 mod day02;
+mod day03;
 
 // Main routine
 fn main() {
@@ -7,4 +8,6 @@ fn main() {
     println!("Day 01 Part 2 Answer = {}", day01::part_2());
     println!("Day 02 Part 1 Answer = {}", day02::part_1());
     println!("Day 02 Part 2 Answer = {}", day02::part_2());
+    println!("Day 03 Part 1 Answer = {}", day03::part_1());
+    println!("Day 03 Part 2 Answer = {}", day03::part_2());
 }
